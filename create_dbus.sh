4c2c79c800e11c6e21dbfbbb4865269eb5716542
Levente Kurusa <levex@linux.com>
2017-01-25 21:05:04 +0000

    create_dbus: add script
    Signed-off-by: Levente Kurusa <levex@linux.com>

---
+++ create_dbus.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ -e "dbus.pid" ]]
+then kill `cat dbus.pid`
+     rm dbus.pid
+fi
+
+raw_session_data=`dbus-daemon --session --print-address --fork --print-pid`
+
+IFS=$'\n' session_data=($raw_session_data)
+
+export DBUS_SESSION_BUS_ADDRESS=${session_data[0]}
+
+echo "${session_data[0]}"
+echo "${session_data[0]}" > session.addr
+echo "${session_data[1]}"
+echo "${session_data[1]}" > dbus.pid
